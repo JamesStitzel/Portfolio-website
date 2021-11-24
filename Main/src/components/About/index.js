a6ccb841b372789fb6735b2230b878f4a3b74465
James Stitzel <duganstitzel@gmail.com>
2021-11-23 20:43:44 -0500

    Establishing structure

---
+++ Main/src/components/About/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="my-5">
+      <div className="my-2">
+        <div className="profile-img my-5">
+        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+        </div>
+        <p>
+        Since I was very young i've had a lifelong interest in technology, and its ability to make life better.  
+         My first real job in the tech industry was an afterschool arangement at a independent computer repair shop in downtown Plant City.
+        </p>
+        <p>
+        I was thrilled
+         to be in contact with more technology than I could inventory. I then proceeded to work at Steak and Shake as a server for about a year, then I 
+         became the store manager of a Subway after only 8 months of working there found a job in the industrial electrical sales industry. After a few 
+         months working in the warehouse, I was moved up into a sales position. But I knew I really wanted was a job in the technology 
+         field, and thats how I got to this point.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
